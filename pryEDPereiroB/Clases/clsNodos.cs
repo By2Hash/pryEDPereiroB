b721ed826a3f9a1f1bbf8c3c0aad694181a2acee
By2Hash <by2git2@gmail.com>
2026-04-16 12:25:35 -0300

    Clase 16/04/2026

---
+++ pryEDPereiroB/Clases/clsNodos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryEDPereiroB
+{
+    internal class clsNodos
+    {
+        private int codigo;
+        private string nombre;
+        private string tramite;
+        private clsNodos siguiente;
+
+        public int Codigo
+        {
+            get { return codigo; }
+            set { codigo =value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Tramite
+        {
+            get { return tramite; }
+            set { tramite = value; }
+        }
+        public clsNodos Siguiente
+        {
+            get { return siguiente; }
+            set { siguiente = value; }
+        }
+
+    }
+}

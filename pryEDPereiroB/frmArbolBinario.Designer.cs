b9228d595ba9a5ddf5d943fc23ab41211d175d13
By2Hash <by2git2@gmail.com>
2026-05-12 11:13:17 -0300

    formulario 'Arbol Binario' hecho

---
+++ pryEDPereiroB/frmArbolBinario.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace pryEDPereiroB
+{
+    partial class frmArbolBinario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmArbolBinario));
+            this.lblListadoArbol = new System.Windows.Forms.Label();
+            this.pnlEliminado = new System.Windows.Forms.Panel();
+            this.cmbCodigo = new System.Windows.Forms.ComboBox();
+            this.lblCodigo1 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lblEliminado = new System.Windows.Forms.Label();
+            this.pnlElementos = new System.Windows.Forms.Panel();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtTramite = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblTramite = new System.Windows.Forms.Label();
+            this.dgvListaDoble = new System.Windows.Forms.DataGridView();
+            this.clmCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmTramite = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblElementos = new System.Windows.Forms.Label();
+            this.btnEquilibrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.optInOrden = new System.Windows.Forms.RadioButton();
+            this.optPreOrden = new System.Windows.Forms.RadioButton();
+            this.optPostOrden = new System.Windows.Forms.RadioButton();
+            this.trvArbolBinario = new System.Windows.Forms.TreeView();
+            this.pnlEliminado.SuspendLayout();
+            this.pnlElementos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaDoble)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblListadoArbol
+            // 
+            this.lblListadoArbol.AutoSize = true;
+            this.lblListadoArbol.Location = new System.Drawing.Point(12, 251);
+            this.lblListadoArbol.Name = "lblListadoArbol";
+            this.lblListadoArbol.Size = new System.Drawing.Size(84, 13);
+            this.lblListadoArbol.TabIndex = 46;
+            this.lblListadoArbol.Text = "Listado del árbol";
+            // 
+            // pnlEliminado
+            // 
+            this.pnlEliminado.Controls.Add(this.cmbCodigo);
+            this.pnlEliminado.Controls.Add(this.lblCodigo1);
+            this.pnlEliminado.Controls.Add(this.btnEliminar);
+            this.pnlEliminado.ForeColor = System.Drawing.Color.Black;
+            this.pnlEliminado.Location = new System.Drawing.Point(463, 28);
+            this.pnlEliminado.Name = "pnlEliminado";
+            this.pnlEliminado.Size = new System.Drawing.Size(231, 111);
+            this.pnlEliminado.TabIndex = 44;
+            // 
+            // cmbCodigo
+            // 
+            this.cmbCodigo.FormattingEnabled = true;
+            this.cmbCodigo.Location = new System.Drawing.Point(88, 21);
+            this.cmbCodigo.Name = "cmbCodigo";
+            this.cmbCodigo.Size = new System.Drawing.Size(121, 21);
+            this.cmbCodigo.TabIndex = 12;
+            // 
+            // lblCodigo1
+            // 
+            this.lblCodigo1.AutoSize = true;
+            this.lblCodigo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo1.Location = new System.Drawing.Point(23, 21);
+            this.lblCodigo1.Name = "lblCodigo1";
+            this.lblCodigo1.Size = new System.Drawing.Size(56, 17);
+            this.lblCodigo1.TabIndex = 11;
+            this.lblCodigo1.Text = "Codigo:";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.IndianRed;
+            this.btnEliminar.Location = new System.Drawing.Point(26, 62);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(183, 30);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            // 
+            // lblEliminado
+            // 
+            this.lblEliminado.AutoSize = true;
+            this.lblEliminado.Location = new System.Drawing.Point(466, 12);
+            this.lblEliminado.Name = "lblEliminado";
+            this.lblEliminado.Size = new System.Drawing.Size(99, 13);
+            this.lblEliminado.TabIndex = 43;
+            this.lblEliminado.Text = "Elemento Eliminado";
+            // 
+            // pnlElementos
+            // 
+            this.pnlElementos.Controls.Add(this.lblCodigo);
+            this.pnlElementos.Controls.Add(this.txtCodigo);
+            this.pnlElementos.Controls.Add(this.txtNombre);
+            this.pnlElementos.Controls.Add(this.txtTramite);
+            this.pnlElementos.Controls.Add(this.btnAgregar);
+            this.pnlElementos.Controls.Add(this.lblNombre);
+            this.pnlElementos.Controls.Add(this.lblTramite);
+            this.pnlElementos.ForeColor = System.Drawing.Color.Black;
+            this.pnlElementos.Location = new System.Drawing.Point(226, 28);
+            this.pnlElementos.Name = "pnlElementos";
+            this.pnlElementos.Size = new System.Drawing.Size(231, 216);
+            this.pnlElementos.TabIndex = 42;
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo.Location = new System.Drawing.Point(23, 35);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(56, 17);
+            this.lblCodigo.TabIndex = 11;
+            this.lblCodigo.Text = "Codigo:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigo.Location = new System.Drawing.Point(109, 35);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 23);
+            this.txtCodigo.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(109, 75);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 23);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtTramite
+            // 
+            this.txtTramite.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTramite.Location = new System.Drawing.Point(109, 117);
+            this.txtTramite.Name = "txtTramite";
+            this.txtTramite.Size = new System.Drawing.Size(100, 23);
+            this.txtTramite.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAgregar.Location = new System.Drawing.Point(26, 155);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(183, 30);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(23, 75);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(62, 17);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblTramite
+            // 
+            this.lblTramite.AutoSize = true;
+            this.lblTramite.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTramite.Location = new System.Drawing.Point(23, 117);
+            this.lblTramite.Name = "lblTramite";
+            this.lblTramite.Size = new System.Drawing.Size(56, 17);
+            this.lblTramite.TabIndex = 5;
+            this.lblTramite.Text = "Tramite";
+            // 
+            // dgvListaDoble
+            // 
+            this.dgvListaDoble.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvListaDoble.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListaDoble.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmCodigo,
+            this.clmNombre,
+            this.clmTramite});
+            this.dgvListaDoble.Location = new System.Drawing.Point(215, 17);
+            this.dgvListaDoble.Name = "dgvListaDoble";
+            this.dgvListaDoble.RowHeadersVisible = false;
+            this.dgvListaDoble.Size = new System.Drawing.Size(453, 209);
+            this.dgvListaDoble.TabIndex = 41;
+            // 
+            // clmCodigo
+            // 
+            this.clmCodigo.HeaderText = "Codigo";
+            this.clmCodigo.Name = "clmCodigo";
+            // 
+            // clmNombre
+            // 
+            this.clmNombre.HeaderText = "Nombre";
+            this.clmNombre.Name = "clmNombre";
+            // 
+            // clmTramite
+            // 
+            this.clmTramite.HeaderText = "Tramite";
+            this.clmTramite.Name = "clmTramite";
+            // 
+            // lblElementos
+            // 
+            this.lblElementos.AutoSize = true;
+            this.lblElementos.Location = new System.Drawing.Point(229, 12);
+            this.lblElementos.Name = "lblElementos";
+            this.lblElementos.Size = new System.Drawing.Size(86, 13);
+            this.lblElementos.TabIndex = 39;
+            this.lblElementos.Text = "Nuevo Elemento";
+            // 
+            // btnEquilibrar
+            // 
+            this.btnEquilibrar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEquilibrar.Location = new System.Drawing.Point(489, 183);
+            this.btnEquilibrar.Name = "btnEquilibrar";
+            this.btnEquilibrar.Size = new System.Drawing.Size(183, 30);
+            this.btnEquilibrar.TabIndex = 47;
+            this.btnEquilibrar.Text = "Equilibrar";
+            this.btnEquilibrar.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.optPostOrden);
+            this.panel1.Controls.Add(this.optPreOrden);
+            this.panel1.Controls.Add(this.optInOrden);
+            this.panel1.Controls.Add(this.dgvListaDoble);
+            this.panel1.Location = new System.Drawing.Point(12, 267);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(682, 238);
+            this.panel1.TabIndex = 48;
+            // 
+            // optInOrden
+            // 
+            this.optInOrden.AutoSize = true;
+            this.optInOrden.Location = new System.Drawing.Point(18, 54);
+            this.optInOrden.Name = "optInOrden";
+            this.optInOrden.Size = new System.Drawing.Size(66, 17);
+            this.optInOrden.TabIndex = 42;
+            this.optInOrden.TabStop = true;
+            this.optInOrden.Text = "In-Orden";
+            this.optInOrden.UseVisualStyleBackColor = true;
+            // 
+            // optPreOrden
+            // 
+            this.optPreOrden.AutoSize = true;
+            this.optPreOrden.Location = new System.Drawing.Point(18, 109);
+            this.optPreOrden.Name = "optPreOrden";
+            this.optPreOrden.Size = new System.Drawing.Size(73, 17);
+            this.optPreOrden.TabIndex = 43;
+            this.optPreOrden.TabStop = true;
+            this.optPreOrden.Text = "Pre-Orden";
+            this.optPreOrden.UseVisualStyleBackColor = true;
+            // 
+            // optPostOrden
+            // 
+            this.optPostOrden.AutoSize = true;
+            this.optPostOrden.Location = new System.Drawing.Point(18, 165);
+            this.optPostOrden.Name = "optPostOrden";
+            this.optPostOrden.Size = new System.Drawing.Size(78, 17);
+            this.optPostOrden.TabIndex = 44;
+            this.optPostOrden.TabStop = true;
+            this.optPostOrden.Text = "Post-Orden";
+            this.optPostOrden.UseVisualStyleBackColor = true;
+            // 
+            // trvArbolBinario
+            // 
+            this.trvArbolBinario.Location = new System.Drawing.Point(12, 28);
+            this.trvArbolBinario.Name = "trvArbolBinario";
+            this.trvArbolBinario.Size = new System.Drawing.Size(200, 216);
+            this.trvArbolBinario.TabIndex = 49;
+            // 
+            // frmArbolBinario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 517);
+            this.Controls.Add(this.trvArbolBinario);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnEquilibrar);
+            this.Controls.Add(this.lblListadoArbol);
+            this.Controls.Add(this.pnlEliminado);
+            this.Controls.Add(this.lblEliminado);
+            this.Controls.Add(this.pnlElementos);
+            this.Controls.Add(this.lblElementos);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmArbolBinario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arbol Binario";
+            this.pnlEliminado.ResumeLayout(false);
+            this.pnlEliminado.PerformLayout();
+            this.pnlElementos.ResumeLayout(false);
+            this.pnlElementos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaDoble)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblListadoArbol;
+        private System.Windows.Forms.Panel pnlEliminado;
+        private System.Windows.Forms.ComboBox cmbCodigo;
+        private System.Windows.Forms.Label lblCodigo1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label lblEliminado;
+        private System.Windows.Forms.Panel pnlElementos;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtTramite;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblTramite;
+        private System.Windows.Forms.DataGridView dgvListaDoble;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmCodigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmTramite;
+        private System.Windows.Forms.Label lblElementos;
+        private System.Windows.Forms.Button btnEquilibrar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton optPostOrden;
+        private System.Windows.Forms.RadioButton optPreOrden;
+        private System.Windows.Forms.RadioButton optInOrden;
+        private System.Windows.Forms.TreeView trvArbolBinario;
+    }
+}
